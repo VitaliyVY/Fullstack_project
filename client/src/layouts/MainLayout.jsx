e0afbc3d5d3fad11a5b31f3e0a4d3c223b7449e2
VitaliyVY <vitaliyverkash@gmail.com>
2026-04-22 16:08:39 +0100

    GTA-fix1

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import AnalyticsTracker from "../components/AnalyticsTracker.jsx";
 
 const MainLayout = () => {
   return (
     <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+      <AnalyticsTracker />
       <Navbar />
       <Outlet />
     </div>
